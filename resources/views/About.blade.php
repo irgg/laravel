38649f87c5f98286bb9cf673ff3a580191444749
irgg <56962236+irgg@users.noreply.github.com>
2019-11-09 18:28:37 +0500

    Add files via upload
    try2

---
+++ resources/views/About.blade.php
@@ -0,0 +1,5 @@
+@extends('Layout')
+@section('title' , 'About')
+@section('navbar')
+@parent
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Hamza Saeed (FA17-BSE-040)</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,11 +81,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hamza Saeed
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
+                    <a href="http://127.0.0.1:8000/About">About</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
